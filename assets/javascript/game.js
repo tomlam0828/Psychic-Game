8a24174e4f653425491095eccec5608eb3f97bb3
Tom Lam <tomlam0828@gmail.com>
2018-10-15 20:14:04 -0700

    .

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,12 @@
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
 var losses = 0;
-var guessesLeft = 9;
-var yourGuess = 9;
+var left = 10;
+var yourGuess = [];
 
 var userWin = document.getElementById("user-win");
 var userLosses = document.getElementById("user-losses");
-var guessesLeft = document.getElementById("guesses-left");
+var userLeft = document.getElementById("user-left");
 var yourGuess = document.getElementById("your-guess");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
@@ -16,11 +16,21 @@ document.onkeyup = function(event) {
     var userGuess = event.key;
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-    if (userGuess) {
+    
         if (userGuess === computerGuess) {
             wins++;
+            userWin.textContent = wins;
         } else if (userGuess != computerGuess) {
             losses++;
-        }
-    }
+            left --;
+            userLosses.textContent = losses;
+            userLeft.textContent = left;
+            if (left < 1) {
+                wins = 0;
+                losses = 0;
+                left = 10;
+            }
+            yourGuess.textContent = userGuess;
+        } 
+        
 }
